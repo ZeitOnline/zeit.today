3a5c35f9be721a9498693890f4f023fe7365b6e3
Wolfgang Schnerring <ws@gocept.com>
2014-06-05 11:46:58 +0200

    Simplify spelling

--- src/zeit/today/tests.py
+++ src/zeit/today/tests.py
@@ -1,25 +1,20 @@
 # Copyright (c) 2007-2012 gocept gmbh & co. kg
 # See also LICENSE.txt
 
-import os
-import unittest
+import pkg_resources
 import zeit.cms.testing
 
 
-TodayLayer = zeit.cms.testing.ZCMLLayer(
-    os.path.join(os.path.dirname(__file__), 'ftesting.zcml'),
-    __name__, 'TodayLayer', allow_teardown=True)
+TodayLayer = zeit.cms.testing.ZCMLLayer('ftesting.zcml')
 
-today_xml_url = 'file://%s' % os.path.join(
-    os.path.dirname(__file__), 'today.xml')
+today_xml_url = 'file://%s' % pkg_resources.resource_filename(
+    __name__, 'today.xml')
 
 
 def test_suite():
-    suite = unittest.TestSuite()
-    suite.addTest(zeit.cms.testing.FunctionalDocFileSuite(
+    return zeit.cms.testing.FunctionalDocFileSuite(
         'README.txt',
         'yesterday.txt',
         layer=TodayLayer,
         product_config={'zeit.today': {'today-xml-url': today_xml_url}}
-    ))
-    return suite
+    )
