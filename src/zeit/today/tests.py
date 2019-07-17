133ab421b194120ed79697464c9fb4f8b6214c66
Wolfgang Schnerring <wolfgang.schnerring@zeit.de>
2019-07-17 13:21:10 +0200

    MAINT: Update to current product config test setup mechanics

--- src/zeit/today/tests.py
+++ src/zeit/today/tests.py
@@ -2,17 +2,20 @@ import pkg_resources
 import zeit.cms.testing
 
 
-TodayLayer = zeit.cms.testing.ZCMLLayer(
-    'ftesting.zcml', product_config=zeit.cms.testing.cms_product_config)
+product_config = """\
+<product-config zeit.today>
+    today-xml-url file://{base}/today.xml
+</product-config>
+""".format(base=pkg_resources.resource_filename(__name__, '.'))
 
-today_xml_url = 'file://%s' % pkg_resources.resource_filename(
-    __name__, 'today.xml')
+TodayLayer = zeit.cms.testing.ZCMLLayer('ftesting.zcml', product_config=(
+    product_config +
+    zeit.cms.testing.cms_product_config))
 
 
 def test_suite():
     return zeit.cms.testing.FunctionalDocFileSuite(
         'README.txt',
         'yesterday.txt',
-        layer=TodayLayer,
-        product_config={'zeit.today': {'today-xml-url': today_xml_url}}
+        layer=TodayLayer
     )
